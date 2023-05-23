b3878fec38b42612129566296c82114bd184d087
jaredgoldman <jaredgoldmandev@gmail.com>
2023-05-22 22:26:32 -0400

    Add dark mode toggle

--- scripts/mode.js
+++ scripts/mode.js
@@ -12,14 +12,12 @@ const darkVars = {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.radio')
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            console.log(button.value)
-            const vars = button.value === 'light' ? lightVars : darkVars
-            Object.entries(vars).forEach(([key, value]) => {
-                document.documentElement.style.setProperty(key, value)
-            })
+    const input = document.querySelector('#mode-checkbox')
+
+    input.addEventListener('change', () => {
+        const vars = input.checked ? lightVars : darkVars
+        Object.entries(vars).forEach(([key, value]) => {
+            document.documentElement.style.setProperty(key, value)
         })
     })
 })
